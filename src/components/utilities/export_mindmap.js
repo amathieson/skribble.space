e5fa26804268e40c6ad3d406c3f8932b50d6817b
Cait <2423179@dundee.ac.uk>
2025-06-26 20:13:37 +0100

    + flippy flip

--- src/components/utilities/export_mindmap.js
+++ src/components/utilities/export_mindmap.js
@@ -1,33 +1,64 @@
-// exportSvgToPdf.js
-
+/**
+ * Exports an SVG element to a PDF file.
+ * The SVG element with the ID 'vector' is parsed into PDF commands
+ * and saved as a downloadable PDF file.
+ * 
+ * @throws Will alert the user if the SVG element with ID 'vector' is not found.
+ * @throws
+ */
 export const exportSvgToPdf = () => {
 
+    //Coords
     let x = 0; 
     let y = 0;
     let w = 0;
     let h = 0;
 
+    // Flip the Y coordinate for a PDF coordinate system
+    let flipY = 0;
+    
+    //Gets the SVG
     const svg = document.getElementById('vector');
+    
+    //TODO: improve user feedback when we have notis of some kind
     if (!svg) {
         alert('SVG map not found!');
         return;
     }
 
+    /**
+     * Converts the background rect to PDF commands
+     * 
+     * Uses the size, etc., of the Vector element to determine the size of the PDF
+     * @param rect
+     * @returns {`${number} ${*} m
+     ${number} ${*} l
+     ${number} ${*} l
+     ${number} ${*} l
+     h
+     S`}
+     */
     const rectToPdfCommands = (rect) => {
         x = parseFloat(rect.getAttribute('x') || 0);
         y = parseFloat(rect.getAttribute('y') || 0);
         w = parseFloat(rect.getAttribute('width') || 0);
         h = parseFloat(rect.getAttribute('height') || 0);
-
-
-        return `${x} ${y} m
-${x + w} ${y} l
-${x + w} ${y + h} l
-${x} ${y + h} l
+        
+        flipY = (y) => h - y;
+        
+        return `${x} ${flipY(y)} m
+${x + w} ${flipY(y)} l
+${x + w} ${flipY(y + h)} l
+${x} ${flipY(y + h)} l
 h
 S`;
     };
 
+    /**
+     * Converts the SVG path to PDF commands
+     * @param d
+     * @returns {string}
+     */
     const parseSvgPathToPdfCommands = (d) => {
         
         
@@ -36,26 +67,35 @@ S`;
         let i = 0;
         while (i < tokens.length) {
             const cmd = tokens[i++];
-            if (cmd === 'M') {
-                const x = tokens[i++];
-                const y = tokens[i++];
-                cmds.push(`${x} ${y} m`);
-            } else if (cmd === 'L') {
-                const x = tokens[i++];
-                const y = tokens[i++];
-                cmds.push(`${x} ${y} l`);
-            } else if (cmd === 'C') {
-                const x1 = tokens[i++];
-                const y1 = tokens[i++];
-                const x2 = tokens[i++];
-                const y2 = tokens[i++];
-                const x3 = tokens[i++];
-                const y3 = tokens[i++];
-                cmds.push(`${x1} ${y1} ${x2} ${y2} ${x3} ${y3} c`);
-            } else if (cmd === 'Z' || cmd === 'z') {
-                cmds.push('h');
-            }
-        }
+            switch (cmd) {
+                case 'M': {
+                    const x = tokens[i++];
+                    const y = tokens[i++];
+                    cmds.push(`${x} ${flipY(y)} m`);
+                    break;
+                }
+                case 'L': {
+                    const x = tokens[i++];
+                    const y = tokens[i++];
+                    cmds.push(`${x} ${flipY(y)} l`);
+                    break;
+                }
+                case 'C': {
+                    const x1 = tokens[i++];
+                    const y1 = tokens[i++];
+                    const x2 = tokens[i++];
+                    const y2 = tokens[i++];
+                    const x3 = tokens[i++];
+                    const y3 = tokens[i++];
+                    cmds.push(`${x1} ${flipY(y1)} ${x2} ${flipY(y2)} ${x3} ${flipY(y3)} c`);
+                    break;
+                }
+                case 'Z':
+                case 'z': {
+                    cmds.push('h');
+                    break;
+                }
+        }}
         cmds.push('S');
         return cmds.join('\n');
     };
@@ -73,6 +113,16 @@ S`;
         contentStream += parseSvgPathToPdfCommands(d) + '\n';
     });
 
+    /**
+     * Generates a basic PDF document as a string using a provided content stream.
+     *
+     * This function creates a minimal PDF structure, including catalogue, pages, and content
+     * objects. The generated PDF content is returned as a single string.
+     *
+     * @param {string} contentStream - The content stream to include in the PDF document. It
+     * should be plain text representing the contents of the PDF.
+     * @returns {string} The PDF document as a string, adhering to the PDF 1.4 specification.
+     */
     const buildPdf = (contentStream) => {
         let objects = [];
         let offsets = [];
@@ -86,6 +136,11 @@ S`;
             return offsets.length;
         };
 
+        /**
+         * YOUR IDE WILL COMPLAIN THESE ARE NOT IN USE, BUT THEY ARE NEEDED FOR THE PDF TO WORK
+         * @type {number}
+         */
+        /* eslint-disable */
         const catalogObjNum = addObject(`1 0 obj
 << /Type /Catalog /Pages 2 0 R >>
 endobj`);
@@ -127,7 +182,8 @@ ${xrefOffset}
         pdf += objects.join('');
         pdf += xref;
         pdf += trailer;
-
+        
+        /* eslint-enable */
         return pdf;
     };
 
@@ -137,6 +193,7 @@ ${xrefOffset}
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
+    //TODO: when we can name the mindmaps, update file name to be the name of the mindmap
     a.download = 'map_export.pdf';
     a.click();
     URL.revokeObjectURL(url);
